31f3e53e1613f290fab26b397ad7bce0c3e64a89
Jamison Griffith <jamison.griffith@gmail.com>
2021-06-12 14:30:57 -0600

    Adds figaro, vcr and webmock for better utilization and testing of APIs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,8 +9,9 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
-require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
+require 'rspec/rails'
+require 'webmock/rspec'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -75,4 +76,12 @@ Shoulda::Matchers.configure do |config|
     with.test_framework :rspec
     with.library :rails
   end
+end
+
+VCR.configure do |config|
+  config.cassette_library_dir = './spec/api_fixtures/vcr_cassettes'
+  config.hook_into :webmock
+  config.filter_sensitive_data('SECRET') { ENV['github-api-key'] }
+  config.configure_rspec_metadata!
+  config.default_cassette_options = { re_record_interval: 3.days }
 end
