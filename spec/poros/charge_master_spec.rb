842c919ca5f2a54c394c4a29f97878329d44a22b
Jamison Griffith <jamison.griffith@gmail.com>
2021-06-14 13:54:50 -0600

    Finish tests for Chargemaster

--- spec/poros/charge_master_spec.rb
+++ spec/poros/charge_master_spec.rb
@@ -15,18 +15,19 @@ RSpec.describe ChargeMaster do
 
   describe '::initialize_discounts' do
     it 'is able to create new discounted items' do
-      ChargeMaster.initialize_discounts(@invoice.id, @merchant.bulk_discounts)
       expect(DiscountedItem.all.empty?).to eq true
+
+      ChargeMaster.initialize_discounts(@invoice.id, @merchant.bulk_discounts)
       correct_placement_1 = DiscountedItem.where("discounted_items.percentage_discount = 0.15")
       correct_placement_2 = DiscountedItem.where("discounted_items.percentage_discount = 0.20")
       correct_placement_3 = DiscountedItem.where("discounted_items.percentage_discount = 0.25")
-      #case_1 = correct_placement_1.all? { |item| item.quantity >= 5 && item.quantity < 8 }
-      #case_2 = correct_placement_2.all? { |item| item.quantity >= 8 && item.quantity < 10 }
-      #case_3 = correct_placement_3.all? { |item| item.quantity >= 10}
+      case_1 = correct_placement_1.all? { |item| item.quantity >= 5 && item.quantity < 8 }
+      case_2 = correct_placement_2.all? { |item| item.quantity >= 8 && item.quantity < 10 }
+      case_3 = correct_placement_3.all? { |item| item.quantity >= 10}
         
-      #expect(case_1).to eq true
-      #expect(case_2).to eq true
-      #expect(case_3).to eq true
+      expect(case_1).to eq true
+      expect(case_2).to eq true
+      expect(case_3).to eq true
     end
   end
 
@@ -41,8 +42,11 @@ RSpec.describe ChargeMaster do
     # Method below is only executed from within ::initialize_discount,
     # So input needs to be pre-sorted
     it 'accounts for checking invoice items against largest discount' do
+      DiscountedItem.destroy_all
       ChargeMaster.initialize_largest_discount(@invoice, @merchant.bulk_discounts.order_by_threshold)
-      expect(DiscountedItem.all.empty?).to eq true
+      correct_placement_3 = DiscountedItem.where("discounted_items.percentage_discount = 0.25")
+      case_3 = correct_placement_3.all? { |item| item.quantity >= 10}
+      expect(case_3).to eq true
     end
   end
 end
