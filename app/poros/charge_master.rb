1c3b10fe51afb616e5230bb86f8abb66e07422c7
Jamison Griffith <jamison.griffith@gmail.com>
2021-06-14 19:08:51 -0600

    Reverting project state to before ChargeMaster refactor

---
+++ app/poros/charge_master.rb
@@ -0,0 +1,47 @@
+class ChargeMaster
+
+  # There's a bug here that will leave out adding an invoice item if its quantity is equal to
+  # the highest bulk discount threshold. This is due to how the quantities between method is written
+  # TODO: Write a method that just selects invoice_items greater or equal to order_by_threshold.last
+  def self.initialize_discounts(invoice_id, discounts)
+    invoice = Invoice.find(invoice_id)
+    ordered_discounts = discounts.order_by_threshold
+    threshold_sets = ChargeMaster.create_subset(ordered_discounts)
+    threshold_sets.each do |set|
+      items = invoice.quantities_between([set.first.quantity_threshold, set.last.quantity_threshold])
+      items.each do |item|
+        DiscountedItem.create!(invoice_item: item, bulk_discount: set.first, 
+                               percentage_discount: set.first.percentage_discount,
+                               quantity: item.quantity)
+      end
+    end
+    # Ideally the below helper method could be refactored to oblivion
+    ChargeMaster.initialize_largest_discount(invoice, ordered_discounts)
+  end
+
+  # Method intakes an invoice and assumes discount parameter is already ordered
+  def self.initialize_largest_discount(invoice, discounts)
+    largest_discount = discounts.last
+    items = invoice.quantities_more_than(largest_discount.quantity_threshold)
+    if !items.empty?
+      items.each do |item|
+        DiscountedItem.create!(invoice_item: item, bulk_discount: largest_discount, 
+                               percentage_discount: largest_discount.percentage_discount,
+                               quantity: item.quantity)
+      end
+    end
+  end
+
+  # There's likely a built-in method that accomplishes this task,
+  # but neither permutations or combinations captured it exactly
+  def self.create_subset(array)
+    return_array = []
+    counter = 0
+    while counter < (array.length - 1) do
+      return_array << [array[counter], array[(counter + 1)]]
+      counter += 1
+    end
+    return_array
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_14_203140) do
+ActiveRecord::Schema.define(version: 2021_06_13_225707) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,7 +38,6 @@ ActiveRecord::Schema.define(version: 2021_06_14_203140) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "quantity"
-    t.integer "unit_price"
     t.index ["bulk_discount_id"], name: "index_discounted_items_on_bulk_discount_id"
     t.index ["invoice_item_id"], name: "index_discounted_items_on_invoice_item_id"
   end
