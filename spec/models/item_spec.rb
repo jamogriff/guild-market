bfe2d697e4e60faaaf148051314ca71b40b8f5a8
Jamison Griffith <jamison.griffith@gmail.com>
2021-07-13 18:41:55 -0600

    Rewrote item model test to check dates instead of object names

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -12,8 +12,8 @@ RSpec.describe Item do
       shippable_items = Item.not_yet_shipped
 
       expect(shippable_items.size).to eq(135)
-      expect(shippable_items.first.name).to eq("Item Hic Soluta")
-      expect(shippable_items.last.name).to eq("Item Ratione Dolor")
+      expect(shippable_items.first.invoice_date.to_s).to eq("2012-03-27 11:54:11 UTC")
+      expect(shippable_items.last.invoice_date.to_s).to eq("2012-03-06 21:54:10 UTC")
     end
   end
 end
